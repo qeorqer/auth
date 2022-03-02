34f5a862efdbd332aede6a70af55e96efe44a0e3
george <heohii.pustovit@ava.codes>
2022-03-02 18:14:27 +0200

    update app.js

--- app.js
+++ app.js
@@ -3,7 +3,26 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 
+const authRouter = require('./routes/auth');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`running on ${PORT}...`));
+app.use(express.json());
+
+app.use(authRouter);
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    app.listen(PORT, () => console.log(`Server is running on ${PORT} port...`))
+  } catch (e){
+    console.log(e);
+  }
+}
+
+start();
