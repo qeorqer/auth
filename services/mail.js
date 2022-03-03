83568040b8cdb2d39c162c2cf7f0cae2791f20d2
george <heohii.pustovit@ava.codes>
2022-03-03 22:41:07 +0200

    create mailing service

---
+++ services/mail.js
@@ -0,0 +1,27 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: false,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASSWORD
+  }
+});
+
+module.exports.sendActivationMail = async (to, link) => {
+  await transporter.sendMail({
+    from: process.env.MAIL_USER,
+    to,
+    subject: 'Account activation',
+    text: '',
+    html:
+      `
+                    <div>
+                        <h1>For activation follow the link</h1>
+                        <a href="${link}">${link}</a>
+                    </div>
+                `
+  })
+}
