98e2e6ddbfb6e3bf9cd119a24f2f2779cb307e2b
george <heohii.pustovit@ava.codes>
2022-03-05 15:00:31 +0200

    add middleware for handling errors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const userRouter = require('./routes/user');
+const errorMiddleware = require('./middlewares/errors-middleware');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use(userRouter);
+app.use(errorMiddleware);
 
 const start = async () => {
   try {
@@ -19,10 +21,10 @@ const start = async () => {
       useUnifiedTopology: true,
     });
 
-    app.listen(PORT, () => console.log(`Server is running on ${PORT} port...`))
-  } catch (error){
+    app.listen(PORT, () => console.log(`Server is running on ${PORT} port...`));
+  } catch (error) {
     console.log(error);
   }
-}
+};
 
 start();
---
+++ middlewares/errors-middleware.js
@@ -0,0 +1,9 @@
+const ApiError = require( '../exceptions/api-errors');
+
+module.exports = (error, req, res, next) => {
+  if (error instanceof ApiError) {
+    return res.status(error.status).json({ message: error.message, errors: error.errors });
+  }
+
+  return res.status(500).json({ message: 'Unexpected server error' });
+};
